28c2d75bf8abe4b30f4be618360f007c468c0c4e
Alexey Pavlikhin <pavlikhin@gmail.com>
2025-06-27 02:27:36 +0300

    Добавлен фон

--- lk.php
+++ lk.php
@@ -60,3 +60,56 @@
 
 <link rel="stylesheet" href="styles.css">
 
+<nav class="t450__menu">
+    <ul role="list" class="t450__list t-menu__list">
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/" data-menu-submenu-hook=""
+                data-menu-item-number="1">
+                Главная
+            </a> </li>
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/uslugi" data-menu-submenu-hook=""
+                data-menu-item-number="2">
+                Услуги
+            </a> </li>
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/kursy-i-vebinary" data-menu-submenu-hook=""
+                data-menu-item-number="3">
+                Курсы и&nbsp;вебинары
+            </a> </li>
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/articles" data-menu-submenu-hook=""
+                data-menu-item-number="4">
+                Статьи
+            </a> </li>
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/events" data-menu-submenu-hook=""
+                data-menu-item-number="5">
+                Мероприятия
+            </a> </li>
+        <li class="t450__list_item"> <a class="t-menu__link-item " href="/kontaktyi" data-menu-submenu-hook=""
+                data-menu-item-number="6">
+                Контакты и вакансии
+            </a> </li>
+    </ul>
+</nav>
+
+<div class="menu">
+    <ul>
+        <li><a href="#">Главная</a></li>
+        <li><a href="#">О компании</a></li>
+        <li><a href="#">Покупателям</a></li>
+        <li>
+            <a class="menu-caret" href="#">Акции</a>
+            <ul>
+                <li><a href="#">Акция 1</a></li>
+                <li><a href="#">Акция 2</a></li>
+                <li><a href="#">Акция 3</a></li>
+                <li><a href="#">Акция 4</a></li>
+                <li><a href="#">Акция 5</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Новости</a></li>
+        <li><a href="#">Контакты</a></li>
+    </ul>
+</div>
+<div > 
+    <img class="tn-atom__img"
+            src="https://static.tildacdn.com/tild3830-3137-4438-b536-396362626530/Mayya_1.png" alt=""
+            imgfield="tn_img_1694086384364"> </div>
+</div>
